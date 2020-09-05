e2c92f7359464903c8b9c5e6fae3a4d7c98297cc
sankethacr <sanketha.cr@reltio.com>
2020-09-05 21:50:36 +0530

    Dockerizing the spring boot application

---
+++ src/main/java/com/docker/DockeriseSpringbootAppApplication.java
@@ -0,0 +1,21 @@
+package com.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockeriseSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockeriseSpringbootAppApplication.class, args);
+	}
+
+	@GetMapping("/message")
+	public String dockerizing() {
+		return "Welcome to dockerizig the application";
+	}
+
+}
